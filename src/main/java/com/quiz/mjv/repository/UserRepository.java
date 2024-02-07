3b0500e1948a98b4c8a4d9927846a7fb854aafb0
Hebert Soares <hebertsoaresof@gmail.com>
2024-02-07 16:06:18 -0300

    Criação do UserRepository

--- src/main/java/com/quiz/mjv/repository/UserRepository.java
+++ src/main/java/com/quiz/mjv/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.quiz.mjv.repository;
 
 import com.quiz.mjv.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
