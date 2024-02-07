e4c4a26fa0119c45389c63fac75835f450c615f3
Hebert Soares <hebertsoaresof@gmail.com>
2024-02-07 17:24:38 -0300

    Update UserService.java

---
+++ src/main/java/com/quiz/mjv/service/UserService.java
@@ -0,0 +1,28 @@
+package com.quiz.mjv.service;
+
+import com.quiz.mjv.entity.User;
+import com.quiz.mjv.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public User signup(User user){
+        return userRepository.save(user);
+    }
+
+
+    @Transactional
+    public List<User> getAllUser(){
+        return userRepository.findAll();
+    }
+
+}
