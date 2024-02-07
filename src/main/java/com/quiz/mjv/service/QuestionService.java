71914faccc49f0eebb046c01cd2dddda20835b2b
Hebert Soares <hebertsoaresof@gmail.com>
2024-02-07 17:29:49 -0300

    Metodo para pegar uma Question Random

--- src/main/java/com/quiz/mjv/service/QuestionService.java
+++ src/main/java/com/quiz/mjv/service/QuestionService.java
@@ -8,6 +8,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+<<<<<<< Updated upstream
+=======
+import java.util.List;
+import java.util.Random;
+
+>>>>>>> Stashed changes
 
 @AllArgsConstructor
 @Service
@@ -21,4 +27,23 @@ public class QuestionService {
         return  questionMapper.toDTO(questionRepository.save(questionMapper.toEntity(questionDTO)));
     }
 
+<<<<<<< Updated upstream
+=======
+    public QuestionDTO getRandomQuestion() {
+        List<Question> allQuestions = questionRepository.findAll();
+        int totalQuestions = allQuestions.size();
+
+        if (totalQuestions == 0) {
+            return null;
+        }
+
+        // Gera um índice aleatório
+        Random random = new Random();
+        int randomIndex = random.nextInt(totalQuestions);
+
+        // Retorna a pergunta aleatória
+        Question randomQuestion = allQuestions.get(randomIndex);
+        return questionMapper.toDTO(randomQuestion);
+    }
+>>>>>>> Stashed changes
 }
