841038157c162a920fe5a161faa291baf53d875d
Hebert Soares <hebertsoaresof@gmail.com>
2024-02-15 15:58:01 -0300

    Criando metodo findAllByOrderByScoreDesc

--- src/main/java/com/quiz/mjv/repository/UserRepository.java
+++ src/main/java/com/quiz/mjv/repository/UserRepository.java
@@ -4,7 +4,13 @@ import com.quiz.mjv.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    User findByNickname(String nickname);
+
+    List<User> findAllByOrderByScoreDesc();
 }
