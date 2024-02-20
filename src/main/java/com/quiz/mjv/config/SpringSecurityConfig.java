0c6553304c1cf64e4bcc3cd5aeff20ca020dfa5a
Hebert Soares <hebertsoaresof@gmail.com>
2024-02-20 19:12:26 -0300

    Instalando JWT

---
+++ src/main/java/com/quiz/mjv/config/SpringSecurityConfig.java
@@ -0,0 +1,11 @@
+package com.quiz.mjv.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableMethodSecurity
+@EnableWebMvc
+@Configuration
+public class SpringSecurityConfig {
+}
