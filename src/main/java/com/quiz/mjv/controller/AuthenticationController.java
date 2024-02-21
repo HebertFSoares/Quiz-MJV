e1f2d0e8639cc4c81d3949e3651ff172fcfedd56
Hebert Soares <hebertsoaresof@gmail.com>
2024-02-21 11:20:21 -0300

    Criando e configurando o AuthenticationController

---
+++ src/main/java/com/quiz/mjv/controller/AuthenticationController.java
@@ -0,0 +1,49 @@
+package com.quiz.mjv.controller;
+
+import com.quiz.mjv.dto.UserAuthDTO;
+import com.quiz.mjv.exception.ErrorMessage;
+import com.quiz.mjv.jwt.JwtToken;
+import com.quiz.mjv.jwt.JwtUserDetailsService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api")
+public class AuthenticationController {
+
+    private final JwtUserDetailsService detailsService;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/auth")
+    public ResponseEntity<?> autenticar(@RequestBody @Valid UserAuthDTO dto, HttpServletRequest request) {
+        log.info("Processo de autenticação pelo login {}", dto.getEmail());
+        try {
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword());
+
+            authenticationManager.authenticate(authenticationToken);
+
+            JwtToken token = detailsService.getTokenAuthenticated(dto.getEmail());
+
+            return ResponseEntity.ok(token);
+        } catch (AuthenticationException ex) {
+            log.warn("Bad Credentials from username '{}'", dto.getEmail());
+        }
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Credenciais Inválidas"));
+    }
+}
