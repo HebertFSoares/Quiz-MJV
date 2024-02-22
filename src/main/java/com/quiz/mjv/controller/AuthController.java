5111000128afc891c03c5a4be9cf41332c3e6a27
Hebert Soares <hebertsoaresof@gmail.com>
2024-02-22 16:14:20 -0300

    Update AuthController.java

--- src/main/java/com/quiz/mjv/controller/AuthController.java
+++ src/main/java/com/quiz/mjv/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.quiz.mjv.controller;
 
 import com.quiz.mjv.service.AuthService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,15 +20,20 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @Operation(summary = "Login", description = "Endpoint para autenticar um usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login bem-sucedido"),
+            @ApiResponse(responseCode = "401", description = "Credenciais inválidas")
+    })
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody Map<String, String> credentials) {
         String nickname = credentials.get("nickname");
         String password = credentials.get("password");
         boolean authenticated = authService.authenticate(nickname, password);
         if (authenticated) {
-            return ResponseEntity.ok("Login successful");
+            return ResponseEntity.ok("Login bem-sucedido");
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
         }
     }
 }
